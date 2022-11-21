67883662e78b05ddfdafbce226fcaec55266f529
lolitakubickaite <lolitakubickaitegmail.com>
2022-11-21 20:01:44 +0200

    correct code

--- hangman_terminal.py
+++ hangman_terminal.py
@@ -2,56 +2,59 @@ import random
 from hangman_body import hangmanBody
 from os import system
 
-
+# making list from txt file 
 my_file = open("little_prince_word_list.txt", "r")
 my_data = my_file.read()
 words = my_data.split()
-print(words)
+
 
 lives = 10
-# entry_valid - set('abcdefghijklmnoprtsuvwxyz')
 
+# greeting with a player
 name = input("Enter your name: ")
 print(f"Hello {name}") 
 print("---------")
-print("Try to guess the word in less than 10 attempts")
+print("Try to guess the word in less than 10 attempts. Good luck!")
 
-def randomWord():
+# generate random word list
+#creating function to play the game
+def randomWord() -> str:
     return words[random.randint(0, len(words) -1)]
 
-def playGame():
+def playGame() -> None:
     word = randomWord()
     progress = ''
     for i in range(len(word)):
         progress += '_'
     incorrect = 0
-    guesses = []
+    tries = []
+
+
     while True:
         _ = system('clear')
-        # print(word)
-        guessesString = ''
-        for i in range(len(guesses)):
-            if i != len(guesses) and i !=0:
-                guessesString += ', '
-            guessesString += guesses[i]
-        print(f'Past guesses: {guessesString}')
+        # clear window game
+        guessesValue = ''
+        for i in range(len(tries)):
+            if i != len(tries) and i !=0:
+                guessesValue += ', '
+            guessesValue += tries[i]
+        print(f'Your guesses: {guessesValue}')
         print(hangmanBody(incorrect))
 
         if progress == word:
             print(progress)
-            print('You win!')
+            print('Congratulation! You win!')
             break
         if incorrect >=10:
             print('You lose! I expected more from you!')
             print(f'Sorry! The word was "{word}". Try again!!! :)')
             break
         print(progress)
-        print('Guess a letter!')
-
+        
         guess = input()
      
-        if guess not in guesses:
-            guesses.append(guess)
+        if guess not in tries:
+            tries.append(guess)
         if guess in word:
             # print(f'The letter {guess} is in the word.')
             for i in range(len(word)):
